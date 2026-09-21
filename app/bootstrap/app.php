d62f6e6470673c2a2b86b20152607ecd7e88b66b
Javier Quiroz <javierquiroztv@gmail.com>
2026-09-21 16:13:35 +0000

    chore: prepare JAKAWI for production proxy

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -15,6 +15,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(
+            at: ['172.18.0.0/16'],
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO,
+        );
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
